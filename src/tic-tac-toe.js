f5b9e38d9e6f929be23d3f514de0474b13be2128
Siarhei Prymakovich <prima.psa@gmail.com>
2019-10-13 04:17:13 +0300

    refactor: rebuilt nextTurn method

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -8,128 +8,95 @@ class TicTacToe {
       this.noturns = false;
       this.field = null;
       this.isdraw = false;
-//console.log('first:' +this.symbol);
+
     }
 
-    getCurrentPlayerSymbol() {
-      
+    getCurrentPlayerSymbol() {      
       return this.symbol;
-
     }
 
     nextTurn(rowIndex, columnIndex) {
-      if(this.matrix[rowIndex][columnIndex]==0 ){
-      this.matrix[rowIndex][columnIndex] = this.symbol;
-       // console.log(this.matrix[rowIndex][columnIndex]);
-      if(this.player == 1) this.player = 2;         
-     
-        
-        for(let i=0; i <3; i++){
-          if(this.matrix[i][0]==this.symbol &&
-            this.matrix[i][1]==this.symbol &&
-            this.matrix[i][2] ==this.symbol){
-            this.win =true;
-            
-          }
-        if(this.matrix[0][i]==this.symbol &&
-            this.matrix[1][i]==this.symbol &&
-            this.matrix[2][i] == this.symbol ){
-           this.win =true;
-           
-           }
-          
-         
-        }
-        if(this.matrix[0][0] == this.symbol &&
-            this.matrix[1][1] == this.symbol &&
-            this.matrix[2][2] == this.symbol ){
-            this.win =true;
-           
-          }
-        
-          if(this.matrix[0][2] == this.symbol &&
-            this.matrix[1][1] == this.symbol &&
-            this.matrix[2][0] == this.symbol ){
-            this.win =true;
-         
-          }
-        if( this.win == true){
-           this.winner = this.symbol;
-        }
-            if(this.symbol == 'x') {this.symbol = 'o';} else {this.symbol = 'x';};
-         
-      
-        }
-     //console.log('win'+this.win+'  curr'+this.symbol);
-      
-     //console.log('row: '+rowIndex+'column: '+columnIndex + '__sym  ' +this.symbol);
+      let column = 0,
+      leftDiagonal = 0,
+      rightDiagonal = 0;
 
+      if(this.matrix[rowIndex][columnIndex]==0 ){
+        this.matrix[rowIndex][columnIndex] = this.symbol;      
+        
+
+        for(let i = 0; i < this.matrix[rowIndex].length; i++ ){
+            let row = 0;
+            if(this.matrix[i][columnIndex] == this.symbol){
+                column++;
+            }
+            if(this.matrix[i][this.matrix[i].length-1-i] === this.symbol){
+                rightDiagonal++;
+            }
+            for(let j = 0; j < this.matrix[i].length; j++){
+                
+                if(this.matrix[rowIndex][j] == this.symbol){
+                    row++;
+                }
+                if( i == j){
+                    if(this.matrix[i][j] == this.symbol){
+                        leftDiagonal++;
+                    }
+                }           
+                if(row === this.matrix[i].length || column === this.matrix[i].length
+                    || leftDiagonal === this.matrix[i].length || rightDiagonal === this.matrix[i].length ){
+                    this.win =true;
+                    break;
+                }             
+            }        
+        }      
+        
+          if( this.win == true)  this.winner = this.symbol;
+          
+          if(this.symbol == 'x') {
+            this.symbol = 'o';
+          } else {
+            this.symbol = 'x';
+          }     
+      }
     }
 
     isFinished() {
-        if(this.winner == null)
-        {for(let i = 0; i <3; i++){
-            if(this.matrix[i].indexOf(0)!=-1){
-              this.win = false;
-              break;
-            }
-            this.win = true;
-            
-        }}
-        return this.win;
 
+        if(this.winner == null && this.noMoreTurns())  this.win = true;                
+        
+        return this.win;
     }
 
     getWinner() {
-    
+
     return this.winner;
-}
+
+    }
 
     noMoreTurns() {
         
-        for(let i = 0; i <3; i++){
+        for(let i = 0; i < this.matrix.length; i++){
             if(this.matrix[i].indexOf(0)!=-1){
               this.noturns = false;
               break;
             }
-            this.noturns = true;
-            
+            this.noturns = true;            
         }
-      return this.noturns;
 
+      return this.noturns;
     }
 
     isDraw() {
         
-        if(this.winner == null){
-            for(let i = 0; i <3; i++){
-                  if(this.matrix[i].indexOf(0)!=-1){
-                    this.isdraw = false;
-                    break;
-                  }
-                  this.isdraw = true;
-                  
-              }
-        // if(this.win == 1) this.isdraw = false;
-      
-    
-}
-      
-      return this.isdraw;
+      if(this.winner == null && this.isFinished()) this.isdraw = true;     
 
+      return this.isdraw;
     }
 
     getFieldValue(rowIndex, colIndex) {
-      return (this.matrix[rowIndex][colIndex] || null);
-      
+      return (this.matrix[rowIndex][colIndex] || null);     
 
     }
 }
-   game = new TicTacToe();
-           
-            
-            
-            
-
 
 module.exports = TicTacToe;
